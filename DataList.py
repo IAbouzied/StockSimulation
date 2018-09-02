65c34634067e5fdaba7a516ebc67a7a6516d0792
[iabouzied] <[ibrahimabouzied@yahoo.com]>
2018-09-01 22:49:22 -0500

    Track last x days in one list

--- DataList.py
+++ DataList.py
@@ -2,37 +2,49 @@ import csv
 from DataPoint import DataPoint
 
 class DataList:
-    'Groups many Data Points together, where functions can be performed'
+    'Groups many Data Points together, iterates through the days and compiles technical data'
 
     def __init__(self, fileLocation, name):
+        #Owned Shares
         self.stockCount = 0
-        self.stockValue = 0
         self.amountSpent = 0
+
         self.day = 0
+
+        #For prividing data on most recent days
         self.last90 = []
-        self.last30 = []
-        self.last15 = []
-        self.last15V = []
+
+        #Moving averages
         self.MA15 = 0
         self.MA30 = 0
         self.MA90 = 0
+
+        #Volumes
+        self.last15V = []
+
+        #Momentum
         self.momentum = 0
         self.volumeMomentum = 0
+
+        #Reading the data from the CSV File
         theCSV = open(fileLocation)
         theReader = csv.reader(theCSV)
         self.DataPoints = []
         for row in theReader:
-            theName = name
-            theDate = row[0]
-            theOpen = row[1]
-            theHigh = row[2]
-            theLow = row[3]
-            theClose = row[4]
-            theVolume = row[5]
-            self.DataPoints.append(DataPoint(theName, theDate, theOpen, theHigh, theLow, theClose, theVolume))
+            name = name
+            date = row[0]
+            openPrice = row[1]
+            high = row[2]
+            low = row[3]
+            close = row[4]
+            volume = row[5]
+            self.DataPoints.append(DataPoint(name, date, openPrice, high, low, close, volume))
         self.DataPoints.reverse()
+
+        #Initial conditions
         self.stockCost = self.DataPoints[self.day].close
         self.volume = self.DataPoints[self.day].volume
+
         self.last15V.append(self.volume)
         self.last90.append(self.stockCost)
         self.stockValue = self.stockCost * self.stockCount
@@ -41,26 +53,29 @@ class DataList:
         return self.DataPoints[key]
 
     def nextDay(self):
+        #Incrementing to most recent data
         self.day += 1
         self.stockCost = self.DataPoints[self.day].close
         self.stockValue = self.stockCost * self.stockCount
         self.volume = self.DataPoints[self.day].volume
+
+        #Update list of most recent days
         self.last90.append(self.stockCost)
-        self.last30.append(self.stockCost)
-        self.last15.append(self.stockCost)
         self.last15V.append(self.volume)
         if len(self.last90) > 90:
                 self.last90.pop(0)
-        if len(self.last30) > 30:
-                self.last90.pop(0)
-        if len(self.last15) > 15:
-                self.last15.pop(0)
         if len(self.last15V) > 15:
                 self.last15V.pop(0)
+
+        #Update Moving averages
         self.MA90 = sum(self.last90) / len(self.last90)
-        self.MA30 = sum(self.last30) / len(self.last30)
-        self.MA15 = sum(self.last15) / len(self.last15)
-        last10 = self.last15[-10:]
+        last30 = self.last90[len(self.last90)-30:len(self.last90)]
+        last15 = self.last90[len(self.last90)-15:len(self.last90)]
+        self.MA30 = sum(last30) / len(last30)
+        self.MA15 = sum(last15) / len(last15)
+
+        #Calculating price momentum
+        last10 = last15[-10:]
         differences = []
         for x in last10:
             if last10.index(x) == 0:
@@ -73,6 +88,7 @@ class DataList:
             else:
                 self.momentum = sum(differences) / len(differences)
 
+        #Calculating volume momentum
         last10V = self.last15V[-10:]
         differencesV = []
         for x in last10V:
